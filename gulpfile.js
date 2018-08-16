638215a6502382063ab27b4bd77adb49ea453000
Bennett Feely <bfeelyweb@gmail.com>
2018-08-15 20:10:25 -0400

    Init project

---
+++ gulpfile.js
@@ -0,0 +1,232 @@
+// Gulp ======================================================================
+gulp = require("gulp");
+
+jade = require("gulp-jade");
+htmlmin = require("gulp-htmlmin");
+
+sass = require("gulp-sass");
+autoprefixer = require("gulp-autoprefixer");
+cssnano = require("gulp-cssnano");
+
+coffee = require("gulp-coffee");
+
+browserSync = require("browser-sync"); 
+readTree = require("./readTree");
+rename = require("gulp-rename");
+const path = require("path");
+const fs = require("fs");
+
+
+// Functions =================================================================
+titleCase = function(str) {
+  str = str.replace(/(^|\b)([a-z])([a-z]+)/g, function(
+    match,
+    space,
+    first,
+    rest
+  ) {
+    return `${space}${first.toUpperCase()}${rest}`;
+  });
+};
+
+capitalize = function(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+function mkDirByPathSync(targetDir, { isRelativeToScript = false } = {}) {
+  const sep = path.sep;
+  const initDir = path.isAbsolute(targetDir) ? sep : "";
+  const baseDir = isRelativeToScript ? __dirname : ".";
+
+  return targetDir.split(sep).reduce((parentDir, childDir) => {
+    const curDir = path.resolve(baseDir, parentDir, childDir);
+    try {
+      fs.mkdirSync(curDir);
+    } catch (err) {
+      if (err.code === "EEXIST") {
+        // curDir already exists!
+        return curDir;
+      }
+
+      // To avoid `EISDIR` error on Mac and `EACCES`-->`ENOENT` and `EPERM` on Windows.
+      if (err.code === "ENOENT") {
+        // Throw the original parentDir error on curDir `ENOENT` failure.
+        throw new Error(`EACCES: permission denied, mkdir '${parentDir}'`);
+      }
+
+      const caughtErr = ["EACCES", "EPERM", "EISDIR"].indexOf(err.code) > -1;
+      if (!caughtErr || (caughtErr && targetDir === curDir)) {
+        throw err; // Throw if it's just the last created dir.
+      }
+    }
+
+    return curDir;
+  }, initDir);
+}
+
+gulp.task("sync", function() {
+  return browserSync({
+    server: {
+      index: "index.html"
+    }
+  });
+});
+
+gulp.task("coffee", function() {
+  return gulp
+    .src("./_coffee/*")
+    .pipe(coffee())
+    .pipe(gulp.dest("./_js/"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
+
+gulp.task("jade", function() {
+  return readTree(function(err, full_list_obj, popular_list_obj, index_list_obj) {
+    if (err) {
+      console.error("Error loading tree", err);
+      return;
+    }
+
+    // Sort the list in categories
+    sorted_full = [];
+    for (title in full_list_obj) {
+      list_data = full_list_obj[title];
+
+      sorted_full.push({ title, list_data });
+    }
+
+    sorted_full.sort(function(a, b) {
+      return a.title.localeCompare(b.title);
+    });
+
+    // Sort the index
+    sorted_index = [];
+    for (filename in index_list_obj) {
+      list_data = index_list_obj[filename];
+      sorted_index.push({ filename, list_data });
+    }
+    sorted_index.sort(function(c, d) {
+      return c.filename.localeCompare(d.filename);
+    });
+
+    // Create folders for each category
+    sorted_full.forEach(function(dir) {
+      var category = dir.title;
+
+      if (!fs.existsSync(dir.title)) {
+        mkDirByPathSync(dir.title);
+        console.log("New 📁 " + dir.title);
+      }
+
+      gulp
+        .src(["./_jade/**/category.jade"])
+        .pipe(
+          jade({
+            pretty: false,
+            data: {
+              category: dir,
+              capitalize
+            }
+          })
+        )
+        .pipe(htmlmin({
+          collapseWhitespace: true
+        }))
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest(dir.title));
+    });
+
+    sorted_index.forEach(function(list) {
+      var list_url = list.list_data.category + "/" + list.list_data.folder;
+
+      if (!fs.existsSync(list_url)) {
+        mkDirByPathSync(list_url);
+        console.log("-- New 📁 " + list_url);
+      }
+
+      gulp
+        .src(["./_jade/**/list.jade"])
+        .pipe(
+          jade({
+            pretty: true,
+            data: {
+              list: list.list_data,
+              capitalize
+            }
+          })
+        )
+        .pipe(htmlmin({
+          collapseWhitespace: true
+        }))
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest(list.list_data.category + "/" + list.list_data.folder));
+    });
+
+    // Run Jade
+    return gulp
+      .src(["./_jade/**/index.jade"])
+      .pipe(
+        jade({
+          pretty: true,
+          data: {
+            full_list: sorted_full,
+            titleCase,
+            capitalize,
+            popular_list: popular_list_obj,
+            titleCase,
+            capitalize,
+            index_list: sorted_index,
+            titleCase,
+            capitalize
+          }
+        })
+      )
+      .pipe(htmlmin({
+        collapseWhitespace: true
+      }))
+      .pipe(gulp.dest("./"))
+      .pipe(
+        browserSync.reload({
+          stream: true
+        })
+      );
+  });
+});
+
+gulp.task("scss", function() {
+  return gulp
+    .src("_scss/style.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(cssnano())
+    .pipe(gulp.dest("_css"))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
+});
+
+gulp.task("default", function() {
+  // gulp.run('coffee', 'jade');
+  gulp.run("coffee", "jade", "scss", "sync");
+  gulp.watch("./_coffee/*", function() {
+    return gulp.run("coffee");
+  });
+  gulp.watch("./_scss/*", function() {
+    return gulp.run("scss");
+  });
+  return gulp.watch(["./_jade/**/*.jade", "./_csv/**/*"], function() {
+    return gulp.run("jade");
+  });
+});
---
+++ readTree.coffee
@@ -0,0 +1,87 @@
+fs = require('fs')
+
+readFileTree = require('readfiletree')
+
+titleize = (str) ->
+    return str.replace(/_/g, ' ').replace('.csv', '');
+
+countProperties = (obj) ->
+    return Object.keys(obj).length;
+
+get = (cb) ->
+  readFileTree '_csv/', (err, tree) ->
+    if err
+      cb(err)
+      return
+
+    full_list = {}
+    index_list = {}
+    popular_list = {}
+
+    for category, comps of tree
+      if category isnt '.DS_Store'
+        
+        full_list[category] = {}
+
+        for filename, comp of comps
+          if filename isnt '.DS_Store'
+
+            full_list[category][filename.toLowerCase()] = {}
+            index_list[filename.toLowerCase()] = {}
+
+
+            if typeof comp is 'string'
+              content_array = comp.split(/\r\n|\r|\n/)
+
+              source_title = undefined
+              source_url = undefined
+              updated = undefined
+
+              # Get the source title and source url
+              if content_array[0].startsWith("[Source: ")
+                source_title = content_array[0].match(/\[([^)]+)\]/)[1].replace("Source: ", "")
+                source_url = content_array[0].match(/\((.*)\)/)[1]
+
+                content_array.shift();
+
+              # Get the updated tag
+              if content_array[0].startsWith("[Updated: ")
+                updated = content_array[0].replace("[Updated: ","").replace("]","")
+                content_array.shift();
+              
+              if content_array[1].startsWith("[Updated: ")
+                updated = content_array[1].replace("[Updated: ","").replace("]","")
+                content_array.shift();
+
+              data = {
+                category: category,
+                category_length: countProperties(full_list[category]),
+                content: content_array,
+                folder: filename.replace('.csv', '').toLowerCase(),
+                length: content_array.length,
+                source_title: source_title,
+                source_url: source_url,
+                title: titleize(filename),
+                updated: updated
+              }
+
+              full_list[category][filename.toLowerCase()] = data
+              index_list[filename.toLowerCase()] = data
+
+              popular_filename_list = [
+                "List_of_Countries_and_Territories.csv",
+                "List_of_US_States.csv",
+                "List_of_Chemical_Elements.csv",
+                "List_of_Time_Zones.csv",
+                "List_of_Books_of_the_Bible.csv",
+                "List_of_US_Presidents.csv"
+              ];
+
+              for popular_filename in popular_filename_list
+                if filename.includes(popular_filename)
+                  popular_list[filename.toLowerCase()] = data;
+
+
+    cb null, full_list, popular_list, index_list
+
+module.exports = get
